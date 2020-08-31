c7e8bd9e13d0c6aa86c822b8edcdfcf1fe8566e5
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2020-08-31 21:18:45 +0200

    made breakpoints

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Search from './Search/Search';
 import Logo from '../../images/logo.svg';
+import menuIcon from '../../images/menu.svg';
 
 import './Header.scss';
 
 const Header = ({setShowSearched}) =>{
     const links = ['Link 1', 'Link 2', 'Link 3', 'My Profile', 'Logout'];
+    const [showNav, setShowNav] = useState(false);
     return (
         <div className='header'>
             <span className='header__logo'>
                 <img src={Logo} alt='logo'/>
             </span>
             <Search setShowSearched={setShowSearched}/>
-            {links.map( link => <span key={link} className='header__link'>{link}</span>)}           
+            <span className={`header__nav ${showNav ? 'menu-open' : ''}`}>
+                {links.map( link => <span key={link} className='header__nav__link'>{link}</span>)}           
+            </span>
+            <img onClick={()=>{setShowNav(!showNav);}} className='header__menu' src={menuIcon} alt='menu'/>
         </div>
     );
 };
